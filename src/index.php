8e90c8c624023635dc41f73b2a3cca6429e91b8d
Moustapha-diop <moistaphadiop57@gmail.com>
2026-02-18 16:17:56 +0000

    Ajout des tests Selenium et de la config Composer

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon App PHP DevOps</title>
+</head>
+<body>
+    <h1 id="main-title">Bienvenue sur mon projet Docker</h1>
+    <p class="description">Ceci est une application testée par Selenium.</p>
+    
+    <img src="1.png" alt="Image de test" id="logo">
+
+    <button id="btn-contact" onclick="alert('Contact cliqué !')">Nous contacter</button>
+</body>
+</html>
---
+++ tests/SeleniumTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+
+class SeleniumTest extends TestCase
+{
+    protected $driver;
+    protected $baseUrl = 'http://localhost:8000';
+
+    // Cette fonction s'exécute AVANT chaque test
+    protected function setUp(): void
+    {
+        $host = 'http://localhost:4444/wd/hub';
+        $capabilities = DesiredCapabilities::chrome();
+        
+        // Options pour le mode "headless" (sans fenêtre, requis pour le DevOps/CI)
+        $capabilities->setCapability('goog:chromeOptions', [
+            'args' => ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
+        ]);
+
+        $this->driver = RemoteWebDriver::create($host, $capabilities);
+    }
+
+    // --- TES TESTS COMMENCENT ICI ---
+
+    public function testIndexContent()
+    {
+        $this->driver->get($this->baseUrl);
+
+        // 1. Vérifier le titre de l'onglet
+        $this->assertEquals('Mon App PHP DevOps', $this->driver->getTitle());
+
+        // 2. Vérifier le titre H1 dans la page
+        $titleH1 = $this->driver->findElement(WebDriverBy::id('main-title'))->getText();
+        $this->assertEquals('Bienvenue sur mon projet Docker', $titleH1);
+
+        // 3. Vérifier que l'image 1.png est présente
+        $image = $this->driver->findElement(WebDriverBy::id('logo'));
+        $this->assertStringContainsString('1.png', $image->getAttribute('src'));
+    }
+
+    // --- TES TESTS S'ARRÊTENT ICI ---
+
+    // Cette fonction s'exécute APRÈS chaque test
+    protected function tearDown(): void
+    {
+        if ($this->driver) {
+            $this->driver->quit();
+        }
+    }
+}
